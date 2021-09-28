960262b01608a685b6e56a9b13e963b8ad6c2835
Angela Sucso <64700259+AngelaSucso@users.noreply.github.com>
2021-09-27 23:58:37 -0500

    Add files via upload

---
+++ practica1/ejercicio2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string ecuacion;
+
+    while(getline(cin,ecuacion)){
+        cout<<ecuacion<<"\n";
+    }
+    return 0;
+}
